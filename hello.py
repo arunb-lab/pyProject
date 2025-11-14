cb1d24c97ae8ef9e25f58cc551bb68d925acafb4
Aroon <baralbhuwan6@gmail.com>
2025-11-14 19:27:41 +0545

    learning pythin and git

--- hello.py
+++ hello.py
@@ -1,1 +1,6 @@
 print("Hello world!")
+print("have a good day!")
+print("learning python is fun!")
+
+name = input('What is your name?') # User types "Kolade" and presses Enter  
+print('Hello', name) # Output: Hello Kolade
