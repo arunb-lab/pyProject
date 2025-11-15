14cefcb2e309b0902b09da3b29866d88c2b132cb
Aroon <baralbhuwan6@gmail.com>
2025-11-15 21:41:50 +0545

    Freecodecamp project Update II

--- main.py
+++ main.py
@@ -4,12 +4,22 @@ empty_dot = '○'
 def create_character(name, strength, intelligence, charisma):
   
     if not isinstance(name, str):
-     return "The character name should be a string."
+     return "The character name should be a string"
 
     if len(name)>10:
-        return "the character name is  too long"
+     return "The character name is too long"
     if " " in name:
         return "The character name should not contain spaces"
+
+    if (not isinstance(strength, int) or 
+        not isinstance(intelligence, int) or 
+        not isinstance(charisma, int)):
+        return "All stats should be integers"
+
+
+    if (strength > 4 or intelligence > 4 or charisma > 4):
+        return "All stats should be no more than 4"
+
 
     return {
         "name": name,
