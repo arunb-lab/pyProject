77a9c5438e47de62bf84b3148d274a691d5e3b12
Aroon <baralbhuwan6@gmail.com>
2025-11-16 08:34:47 +0545

    Common Operations used with Integers and Floats

---
+++ operations.py
@@ -0,0 +1,24 @@
+int_1 = 56
+int_2 = 12
+float_1 = 5.4
+float_2 = 12.0
+
+# Addition
+
+print('Integer Addition:', int_1 + int_2) # Integer Addition: 68
+print('Float Addition:', float_1 + float_2) # Float Addition: 17.4
+
+# Subtraction
+
+print('Int Subtraction:', int_1 - int_2) # Int Subtraction: 44
+print('Float Subtraction:',  float_2 - float_1) # Float Subtraction: 6.6
+
+# Multiplication
+
+print('Int Multiplication:', int_1 * int_2) # Int Multiplication: 672
+print('Float Multiplication:', float_2 * float_1) # Float Multiplication: 64.80000000000001
+
+# Division
+
+print('Int Division:', int_1 / int_2) # Int Division: 4.666666666666667
+print('Float Division:', float_2 / float_1) # Float Division: 2.222222222222222
