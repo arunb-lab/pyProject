7bbc4470846ab5b84580a92c63e24d8dc1406808
Aroon <baralbhuwan6@gmail.com>
2025-11-17 20:40:58 +0545

    learning loops in Py for loop

--- loops.py
+++ loops.py
@@ -2,3 +2,25 @@ programming_languages = ['Rust', 'Java', 'Python', 'C++']
 
 for language in programming_languages:
     print(language)
+ ### Output:
+# Rust
+# Java
+# Python            
+# C++   
+
+for char in 'Data':
+    print(char)
+ ### Output:
+# D
+# a
+# t     
+
+secret_number = 3
+guess = 0
+
+while guess != secret_number:
+    guess = int(input('Guess the number (1-5): '))
+    if guess != secret_number:
+        print('Wrong! Try again.')
+
+print('You got it!')
