7a5f7556c34fde2aa68050dc118460e3ca6db993
Aroon <baralbhuwan6@gmail.com>
2025-11-18 19:51:07 +0545

    index and value print using Python loop

--- pyzip.py
+++ pyzip.py
@@ -5,3 +5,16 @@ index = 0
 for language in languages:
     print(f'Index {index} and language {language}')
     index += 1
+
+# Using enumerate to achieve the same result
+languages = ['Spanish', 'English', 'Russian', 'Chinese']
+
+list(enumerate(languages))
+# [(0, 'Spanish'), (1, 'English'), (2, 'Russian'), (3, 'Chinese')]
+for index, language in enumerate(languages):
+    print(f'Index {index} and language {language}')
+# Starting the index from 1 instead of 0
+languages = ['Spanish', 'English', 'Russian', 'Chinese']
+
+for index, language in enumerate(languages, 1):
+    print(f'Index {index} and language {language}')
