63b8cadfb400d26727eb92390aa127f8ce18d1c0
Aroon <baralbhuwan6@gmail.com>
2025-11-23 21:52:55 +0545

    pin extractor freecodecamp  project loops

--- projectapp.py
+++ projectapp.py
@@ -21,3 +21,22 @@ pin_extractor(poem)
 
 #nothig today beacuse it's saturday this isfor github streak
 #nothig today beacuse it's saturday this isfor github streak
+
+#freecodecamp.py
+def caesar(text, shift, encrypt=True):
+
+    if not isinstance(shift, int):
+        return 'Shift must be an integer value.'
+
+    if shift < 1 or shift > 25:
+        return 'Shift must be an integer between 1 and 25.'
+
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+
+    if not encrypt:
+        shift = - shift
+    
+    shifted_alphabet = alphabet[shift:] + alphabet[:shift]
+    translation_table = str.maketrans(alphabet + alphabet.upper(), shifted_alphabet + shifted_alphabet.upper())
+    encrypted_text = text.translate(translation_table)
+    return encrypted_text
