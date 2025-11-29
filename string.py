e4656412007557bd7807e8795e1d7d89ecd12e18
Aroon <baralbhuwan6@gmail.com>
2025-11-29 21:02:31 +0545

    check palindrome or not

---
+++ string.py
@@ -0,0 +1,15 @@
+s = "neveroddoreven"  
+i, j = 0, len(s) - 1  
+is_palindrome = True  
+
+while i < j:
+    if s[i] != s[j]:  
+        is_palindrome = False
+        break
+    i += 1
+    j -= 1
+
+if is_palindrome:
+    print("Yes a palindrome") 
+else:
+    print("Not a palindrome")
